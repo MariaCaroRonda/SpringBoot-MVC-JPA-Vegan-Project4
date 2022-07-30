b669a2ad54d85637bcec531d0b6261ce790f7672
Maria Caro <github.maria.caro@gmail.com>
2022-07-30 14:32:24 +0100

    New MVC JPA Project

---
+++ src/main/java/com/springboot/vegan/SpringBootMvcJpaVeganProject4Application.java
@@ -0,0 +1,13 @@
+package com.springboot.vegan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcJpaVeganProject4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcJpaVeganProject4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/vegan/SpringBootMvcJpaVeganProject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.vegan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcJpaVeganProject4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
