33a20bd07437892b3c88bd8f0700ebbedb195409
Maria Caro <github.maria.caro@gmail.com>
2022-07-30 18:55:09 +0100

    Add @RequestMapping at class level in CategoriesController Create RecipesController and detail.html

--- src/main/java/com/springboot/vegan/controller/CategoriesController.java
+++ src/main/java/com/springboot/vegan/controller/CategoriesController.java
@@ -1,4 +1,32 @@
 package com.springboot.vegan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/categories")
 public class CategoriesController {
+
+    // @GetMapping
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String showIndex(Model model) {
+
+        return "categories/listCategories";
+    }
+
+    // @GetMapping
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String create() {
+        return "categories/formCategory";
+    }
+
+    // @PostMapping
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save() {
+        return "categories/listCategories";
+    }
 }
