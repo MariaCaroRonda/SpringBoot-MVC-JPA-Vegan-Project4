83adad68ed12e607130d41ad3600d61d1573f520
Maria Caro <github.maria.caro@gmail.com>
2022-07-31 20:31:22 +0100

    Test CategoriesRepository in the main class 'SpringBootMvcJpaVeganProject4Application'

--- src/main/java/com/springboot/vegan/SpringBootMvcJpaVeganProject4Application.java
+++ src/main/java/com/springboot/vegan/SpringBootMvcJpaVeganProject4Application.java
@@ -1,11 +1,16 @@
 package com.springboot.vegan;
 
+import com.springboot.vegan.repository.CategoriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootMvcJpaVeganProject4Application implements CommandLineRunner {
+
+    @Autowired
+    private CategoriesRepository categoriesRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMvcJpaVeganProject4Application.class, args);
@@ -14,7 +19,9 @@ public class SpringBootMvcJpaVeganProject4Application implements CommandLineRunn
     @Override
     public void run(String... args) throws Exception {
         /*System.out.println("Examples of Spring Data JPA");*/
-        save();
+        System.out.println(categoriesRepository);
+
+       // save();
     }
 
     private void save() {
---
+++ src/main/java/com/springboot/vegan/repository/CategoriesRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.vegan.repository;
+
+import com.springboot.vegan.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriesRepository extends CrudRepository<Category, Integer> {
+}
