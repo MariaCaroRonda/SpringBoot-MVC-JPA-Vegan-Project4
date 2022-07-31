15ce9a741920d9f61efa080f6e30b18c4286a517
Maria Caro <github.maria.caro@gmail.com>
2022-07-31 21:04:23 +0100

    Test 'update()' in the main class implementing CommandLineRunner

--- src/main/java/com/springboot/vegan/SpringBootMvcJpaVeganProject4Application.java
+++ src/main/java/com/springboot/vegan/SpringBootMvcJpaVeganProject4Application.java
@@ -23,9 +23,28 @@ public class SpringBootMvcJpaVeganProject4Application implements CommandLineRunn
 
     @Override
     public void run(String... args) throws Exception {
-        /*save();*/
+        /* save();
+           findById();  */
 
-        findById();
+        update();
+
+    }
+
+    /* If the category exists, it updates the category
+    * If the category doesn't exist, it creates a new one */
+    private void update() {
+        Optional<Category> optional = categoriesRepository.findById(3);
+        if (optional.isPresent()) {
+            Category tmpCat = optional.get();
+            tmpCat.setName("Snacks");
+            tmpCat.setDescription("A small amount of food eaten between meals.");
+            categoriesRepository.save(tmpCat);
+            System.out.println(optional.get());
+        }
+        else
+        {
+            System.out.println("Category not found");
+        }
     }
 
     private void findById() {
